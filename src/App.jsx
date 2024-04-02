43a7d62239fe66ec62574cd54f34a659157ae629
Jemish1306 <128604444+Jemish1306@users.noreply.github.com>
2024-04-02 13:19:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+const App = () => {
+
+  
+  return (
+    <>
+    
+    {/* <div className='w-full h-full'><iframe src='C:\My-Portfolio-main\protfoliyomain\jsport\public\index.html'  title="Large File"
+        width="100%"
+        height="800px"
+        frameBorder="0"/></div> */}
+    </>
+  )
+}
+
+export default App
